2a5da1d8c9599ba46978b0269160001b7b90a0b1
shaolin-wu <15265249516@163.com>
2018-01-21 16:53:17 +0800

    What: V2.0 添加JS脚本发起请求
    Why:
    
    * V1使用image不方便
    
    How:
    
    *
    直接把js脚本添加到body里面,错误处理在这.成功处理直接在index.js服务器代码

--- index.js
+++ index.js
@@ -34,15 +34,14 @@ var server = http.createServer(function(request, response){
   }else if(path === '/pay') {
     var amount = fs.readFileSync('./db', 'utf8')
     var newAmount = amount - 1
-    if(Math.random() > 0.5) {
-      fs.writeFileSync('./db', newAmount)
-      response.setHeader('Content-Type', 'image/jpg')
-      response.statusCode = 200
-      response.write(fs.readFileSync('./dog.jpg'))
-    } else {
-      response.statusCode = 400
-      response.write('failed')
-    }
+    
+    fs.writeFileSync('./db', newAmount)
+    response.setHeader('Content-Type', 'application/javascript')
+    response.statusCode = 200
+    response.write(`
+      amount.innerText = amount.innerText - 1
+    `)
+
     response.end()
   }else{  
     response.statusCode = 404
