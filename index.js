4fd57d5c686820cf743e5e6a9669ddbd92e4b405
shaolin-wu <15265249516@163.com>
2018-01-22 20:54:17 +0800

    What:V3.0 模拟JSONP的雏形
    Why:
    
    * 前后端的解耦
    
    How:
    
    *前端在html里面动态写入js脚本，src指向服务器，同时附带一个查询参数，服务器接受请求后会执行这个函数

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var url = require('url')
 //console.log(Object.keys(http))
 var port = process.env.PORT || 8888;
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
 
   var temp = url.parse(request.url, true)
   var path = temp.pathname
@@ -14,38 +14,38 @@ var server = http.createServer(function(request, response){
 
   //从这里开始看，上面不要看
 
-  if(path === '/'){  // 如果用户请求的是 / 路径
-    var string = fs.readFileSync('./index.html', 'utf8')  
+  if (path === '/') {  // 如果用户请求的是 / 路径
+    var string = fs.readFileSync('./index.html', 'utf8')
     var amount = fs.readFileSync('./db', 'utf-8')
     string = string.replace('&&&amount&&&', amount)
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')  
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
-    response.end()   
-  }else if(path === '/style.css'){   
+    response.end()
+  } else if (path === '/style.css') {
     var string = fs.readFileSync('./style.css', 'utf8')
     response.setHeader('Content-Type', 'text/css')
     response.write(string)
-    response.end()  
-  }else if(path === '/main.js'){  
+    response.end()
+  } else if (path === '/main.js') {
     var string = fs.readFileSync('./main.js', 'utf8')
     response.setHeader('Content-Type', 'application/javascript')
     response.write(string)
     response.end()
-  }else if(path === '/pay') {
+  } else if (path === '/pay') {
     var amount = fs.readFileSync('./db', 'utf8')
     var newAmount = amount - 1
-    
+
     fs.writeFileSync('./db', newAmount)
     response.setHeader('Content-Type', 'application/javascript')
     response.statusCode = 200
     response.write(`
-      amount.innerText = amount.innerText - 1
+      ${query.callback}.call(undefined, 'success')
     `)
 
     response.end()
-  }else{  
+  } else {
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8') 
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write('找不到对应的路径，你需要自行修改 index.js')
     response.end()
   }
